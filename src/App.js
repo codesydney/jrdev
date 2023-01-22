32dcc286f054b5f211f1006bc8be1ee7365c16f2
NimaSakha <103934713+NimaSakha@users.noreply.github.com>
2023-01-22 21:10:40 +1100

    code update
    Init React App.

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import SignUp from "./components/SignUp"
+function App() {
+  return (
+    <>
+      <SignUp />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,104 @@
+import { useState } from "react"
+function SignUp() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [passwordConfirm, setPasswordConfirm] = useState("")
+  const [error, setError] = useState("")
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value)
+  }
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
+  const handlePasswordConfirm = (e) => {
+    setPasswordConfirm(e.target.value)
+  }
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const data = { name, email, password, passwordConfirm }
+    handlePost(data)
+  }
+
+  const handlePost = (user) => {
+    const url = "https://jrdevau.herokuapp.com/api/v1/users/signUp"
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    }).then((result) => {
+      result.json().then((resp) => {
+        setError(resp.message)
+      })
+    })
+  }
+
+  return (
+    <div className='text-center center'>
+      <div className='signin'>
+        <div className='form-signin w-100 m-auto gap-3'>
+          <form onSubmit={handleSubmit}>
+            <h1 className='h3 mb-3 fw-normal'>Please sign up</h1>
+            <div className='form-floating'>
+              <input
+                type='text'
+                className='form-control rounded-0 rounded-top'
+                id='floatingInput'
+                placeholder='Name'
+                onChange={handleName}
+                value={name}
+              />
+              <label htmlFor='floatingInput'>Name</label>
+            </div>
+            <div className='form-floating'>
+              <input
+                type='email'
+                className='form-control rounded-0 '
+                id='floatingInput'
+                placeholder='name@example.com'
+                onChange={handleEmail}
+                value={email}
+              />
+              <label htmlFor='floatingInput'>Email address</label>
+            </div>
+            <div className='form-floating'>
+              <input
+                type='password'
+                className='form-control rounded-0'
+                id='floatingPassword'
+                placeholder='Password'
+                onChange={handlePassword}
+                value={password}
+              />
+              <label htmlFor='floatingPassword'>Password</label>
+            </div>
+            <div className='form-floating mb-5'>
+              <input
+                type='password'
+                className='form-control rounded-0 rounded-bottom shadow mb-1'
+                id='floatingPassword'
+                placeholder='Confirm Password'
+                onChange={handlePasswordConfirm}
+                value={passwordConfirm}
+              />
+              <label htmlFor='floatingPassword'>Confirm Password</label>
+              <p className='text-danger'>{error}</p>
+            </div>
+
+            <button className='w-100 btn btn-lg btn-primary' type='submit'>
+              Sign up
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SignUp
