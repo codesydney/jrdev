193fbfa54cb98186b106ec09055f23da9373511f
Jimmy Yao <jimmyyaocs@gmail.com>
2023-03-25 23:00:13 +1100

    Refacotr: update loading page with spin animation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './index.css';
 import { UserProvider } from './context/UserInfo';
 import ReactDOM from 'react-dom/client';
 import App from './App';
--- src/page/Loading.jsx
+++ src/page/Loading.jsx
@@ -1,23 +1,16 @@
-// import React from 'react';
-
-// const Loading = () => {
-//   return (
-//     <div>
-//       <h1>Loading....</h1>
-//     </div>
-//   );
-// };
-
-// export default Loading;
-
 import React from 'react';
 
-function Loading() {
+const Loading = () => {
   return (
-    <>
-      <h1>Loading...</h1>
-    </>
+    <div className="flex h-screen">
+      <div className="m-auto">
+        <div className="border-t-8 border-b-8 border-gray-400 h-16 w-16 rounded-full animate-spin"></div>
+        <div className="mt-8 text-center">
+          <p className="text-gray-600 text-lg font-semibold">Loading...</p>
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default Loading;
