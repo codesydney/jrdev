4ace4623587912d8faa354814b3fceebb7f15397
NimaSakha <103934713+NimaSakha@users.noreply.github.com>
2023-04-10 17:04:26 +1000

    delete repo content for NEXT.JS

--- src/Hooks/useUserContext.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import UserContext from '../context/UserInfo';
-
-export const useUserContext = () => {
-  const { user, setUser, isLoggedIn, setIsLoggedIn } = useContext(UserContext);
-
-  return user, setUser, isLoggedIn, setIsLoggedIn;
-};
