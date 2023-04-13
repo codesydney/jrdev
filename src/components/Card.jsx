9c735414286ad74872cd4ea70d33b4ad7e1f3abc
NimaSakha <103934713+NimaSakha@users.noreply.github.com>
2023-04-13 19:40:18 +1000

    Clean up card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,17 @@
 import React from "react"
-import Link from "next/link"
+
 import { signIn } from "next-auth/react"
 function Card({ title, text }) {
   return (
-    <div className=' min-w-[300px] border-[3px] border-primary p-5 mt-10 md:mx-0 mx-24 px-3 rounded-3xl flex flex-col items-center'>
-      <h2 className='m-0 text-center'>{title}</h2>
-      <hr className='m-0 mb-3' />
-      <p className='text-center'>{text}</p>
+    <div className=' min-w-[300px] min-h-[250px] border-[3px] border-primary p-5 mt-10 md:mx-0 mx-24 px-3 rounded-3xl flex flex-col justify-between items-center'>
+      <div>
+        <h2 className='m-0 text-center pb-8'>{title}</h2>
+
+        <p className='text-center'>{text}</p>
+      </div>
+
       <button
-        className='mt-32 font-bold no-underline text-xl bg-primary px-5 py-1 rounded-full text-white shadow-lg'
+        className=' font-bold no-underline text-xl bg-primary px-5 py-1 rounded-full text-white shadow-lg'
         onClick={() => signIn()}
       >
         Sign up
