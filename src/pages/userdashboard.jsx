35447e0fabf2c4ca0e882ec0286bed0bbd6caec7
Nima Sakha <nimasakha@gmail.com>
2023-04-25 16:01:02 +1000

    Update userdashboard.jsx

--- src/pages/userdashboard.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import useUserhandler from "@/hooks/useUserhandler"
-function userdashboard() {
-  useUserhandler()
-  return <div>This is the user dashboard</div>
-}
-
-export default userdashboard
